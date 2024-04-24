f614092e08484c0b343bf23231fcc24a6c3c8ec0
ubuntu <piotr.perz@wp.pl>
2024-04-24 22:49:02 +0200

    Day 23: Send email using the form; foto changed

--- Home.py
+++ Home.py
@@ -4,7 +4,7 @@ import pandas
 st.set_page_config(layout="wide")
 col1, col2 = st.columns(2)
 with col1:
-    st.image("images/photo.png")
+    st.image("images/1-perz.JPG")
 
 with col2:
     st.title("Piotr Perz")
--- send_email.py
+++ send_email.py
@@ -1,20 +1,15 @@
 import smtplib, ssl, time
 
-host = "smtp.gmail.com"
-port = 465
+def send_email(message):
+    host = "smtp.gmail.com"
+    port = 465
 
-username = "perzak102@gmail.com"
-password = "fzgqivtksrnlspuk"
+    username = "perzak102@gmail.com"
+    password = "fzgqivtksrnlspuk"
 
-receiver = "perzak102@wp.pl"
-context = ssl.create_default_context()
+    receiver = "perzak102@wp.pl"
+    context = ssl.create_default_context()
 
-message = """\
-Subject: Hi!
-How are you?
-Bye!
-"""
-
-with smtplib.SMTP_SSL(host, port, context=context) as server:
-    server.login(username, password)
-    server.sendmail(username, receiver, message)
+    with smtplib.SMTP_SSL(host, port, context=context) as server:
+        server.login(username, password)
+        server.sendmail(username, receiver, message)
