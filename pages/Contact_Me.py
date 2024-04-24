35b1081496daa9b95832f2d8911f63204981af1a
ubuntu <piotr.perz@wp.pl>
2024-04-24 23:22:24 +0200

    Day 23: Add topic to form

--- pages/Contact_Me.py
+++ pages/Contact_Me.py
@@ -1,15 +1,21 @@
+import pandas
 import streamlit as st
 from send_email import send_email
 
 st.header("Contact Me")
+df = pandas.read_csv("topics.csv")
 
 with st.form(key="email_form"):
     user_email = st.text_input("Your email address")
+    topic = st.selectbox(
+        "What topic you want to discuss?",
+        df['topic'])
     raw_message = st.text_area("Your message")
-    message = f"""\
+    message: str = f"""\
 Subject: New email from {user_email}
-    
+
 From: {user_email}
+Topic: {topic}
 {raw_message}
 """
     button = st.form_submit_button("Submit")
